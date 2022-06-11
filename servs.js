c795f3f3f729160c705fc1c733d7f2caa652d656
nindozzmo <80521361+nindozzmo@users.noreply.github.com>
2022-06-11 05:55:45 +0300

    Update servs.js

--- servs.js
+++ servs.js
@@ -1,80 +1,178 @@
 /** @param {NS} ns */
 
-//Code by Nindozzmo
+// Code by Nindozzmo
 
 // This is a script for purchased servers batch operations.
 
-// How to use: terminal: run servs.js [action] [arg1] [arg2]
+//////// v1.2 
+//////// changelog:
+//////// 1. added checks for input data type for an actions names;
+//////// 2. added checks for input data type for a buy, copy and run actions arguments;
+//////// 3. changed behaviour of the run function;
+//////// 4. changed manual below for the run action.
+
+//////////TODO for v1.3:
+////////// 1. add choise of the range of servers for a kill, run, copy(?) and del functions;
+////////// 2. get rid of the copy function;(?)
+////////// 3. add delays to the run function hack, grow and weaken;
+////////// 4. change behaviour of the run function (copy hacktools.js instead of any file);
+////////// 5. rename the run function (name should be 'hack').
+
+// How to use:
+// terminal: run servs.js [action] [argument 1] [argument 2]
 
 // Actions and arguments list:
 
-// buy [RAM] — buy maximum amount of servers with [RAM] amount of memory;
+/// buy [RAM] — buy maximum amount of servers with [RAM] amount of memory;
 
-// run [file] [threads] — kill all running scripts on each purchased server, copy [file]
-// from your home server and run it at the number of [threads];
+/// copy [file] — copy selected file on all purchased servers;
 
-// kill — stop all running scripts on each purchased server;
+/// run [file] [attacked server] — kill all running scripts on each purchased server, copy file from your
+/// home server and run it in in grow, weaken and hack modes with recommended amounts of threads (10, 2 and 1 parts) on each
+/// purchased server; overall amount of threads for each server is based on each purchased server's RAM;
 
-// del — delete all purchased servers;
+/// kill — stop all running scripts on each purchased server;
+
+/// del — delete all purchased servers;
 
 export async function main(ns) {
-	const choise = ns.args[0];
-	const arg2 = ns.args[1];
-	const arg3 = ns.args[2];
+	const action = ns.args[0];
+	const arg1 = ns.args[1];
+	const arg2 = ns.args[2];
 
 	const servs = ns.getPurchasedServers();
-	const max = ns.getPurchasedServerLimit();
+	const maxServs = ns.getPurchasedServerLimit();
 
-	function buy(ram) {
-		for (let i = servs.length; i < max; i++) {
-			ns.purchaseServer("serv-" + i, ram)
-		}
-	}
+	function buy(ram) { //buy servers
+		let overallBought;
 
-	function kill() {
-		for (let i = 0; i < servs.length; i++) {
-			ns.killall(servs[i]);
-		}
-	}
+		if (!isNaN(ram) && ram != 0) { //check if ram is a number and not a 0
 
-	function del() {
-		for (let i = 0; i < servs.length; i++) {
-			ns.deleteServer(servs[i]);
-		}
-	}
+			for (let i = servs.length; i < maxServs; i++) { //buy loop
+				if (ns.getPurchasedServerCost(ram) < ns.getServerMoneyAvailable('home')) {
+					ns.purchaseServer("serv-" + i, ram);
+					overallBought += 1;
+				}
 
-	async function run(file, threads) {
-		const scriptram = ns.getScriptRam(file, "home");
+				else {
+					ns.tprint("You don't have enough money");
+					break;
+				}
 
-		for (let i = 0; i < servs.length; i++) {
-			let servram = ns.getServerMaxRam(servs[i]);
+			}
 
-			if (servram > scriptram * threads) {
-				ns.killall(servs[i]);
-				await ns.scp(file, "home", servs[i]);
-				ns.exec(file, servs[i], threads);
+			if (overallBought < 2 && overallBought != 0) {
+				ns.tprint("You've bought " + overallBought + " server")
 			}
 
 			else {
-				ns.tprint("Server " + servs[i] + " have " + servram + " RAM. ",
-					"Script " + file + " at " + threads + " threads needs " + (scriptram * threads) + " of free RAM")
+				ns.tprint("You've bought " + overallBought + " servers")
+			}
+
+		}
+
+		else {
+			ns.tprint("RAM amount must be a number!");
+		}
+	}
+
+	function kill() { //kill all scripts running on the purchased servers
+		for (let i = 0; i < servs.length; i++) {
+			ns.killall(servs[i]);
+		}
+		ns.tprint("You've killed all running scripts on every purchased server");
+	}
+
+	function del() { //delete all purchased servers
+		if (servs.length != 0) {
+			for (let i = 0; i < servs.length; i++) {
+				ns.deleteServer(servs[i]);
+			}
+			ns.tprint("You've deleted all purchased servers");
+		}
+
+		else {
+			ns.tprint("You don't have any purchased servers");
+		}
+	}
+
+	async function copy(file) { //copy file on all purchased servers
+		for (let i = 0; i < servs.length; i++) {
+			if (typeof file === "string" && file !== "" && file !== " " && !ns.fileExists(file, 'home')) { //check if file exists on "home"
+				ns.tprint("file '" + file + "' doesn't exist on 'home'!");
+				break;
+			}
+
+			else if (typeof file !== "string" || file == "" || file == " ") { //check if file name is a string
+				ns.tprint("File name should be a string");
+				break;
+			}
+
+			else { //copy loop
+				await ns.scp(file, "home", servs[i]);
 			}
 		}
 	}
 
-	if (choise == "buy") {
-		buy(arg2);
+	async function run(file, server) { //kill all scripts, copy hacktools.js and run it on all purchased servers
+		for (let i = 0; i < servs.length; i++) { //loop for all purchased servers
+
+			if (typeof file === "string" && file !== "" && file !== " " && !ns.fileExists(file, 'home')) { //check if file exists on "home"
+				ns.tprint("file '" + file + "' doesn't exist on 'home'!");
+				break;
+			}
+
+			else if (typeof file !== "string" || file == " " || file == "") { //check if file name is a string
+				ns.tprint("File name should be a string");
+				break;
+			}
+
+			else if (typeof server !== "string" || server == " " || server == "") { //check if server name is a string
+				ns.tprint("Server name should be a string");
+				break;
+			}
+
+			else if (!ns.serverExists(server)) { //check if server exists
+				ns.tprint("Server " + server + " doesn't exist");
+				break;
+			}
+
+			else {
+				const threads = (ns.getServerMaxRam(servs[i]) / ns.getScriptRam(file, servs[i])) / 13; //calculate one part of threads
+
+				ns.killall(servs[i]);
+				await ns.scp(file, "home", servs[i]);
+				ns.exec(file, servs[i], threads * 10, 'grow', server); //ten parts threads with grow
+				ns.exec(file, servs[i], threads * 2, 'weaken', server); //two parts threads with weaken
+				ns.exec(file, servs[i], threads, 'hack', server);
+			}
+		}
 	}
 
-	else if (choise == "run") {
-		await run(arg2, arg3);
+	if (typeof action !== "string" || action == " " || action == "") { //check if action type is a valid string
+		ns.tprint("Action name should be a string");
 	}
 
-	else if (choise == "kill") {
-		kill();
-	}
+	else {
 
-	else if (choise == "del") {
-		del();
+		if (action == "buy") {
+			buy(arg1);
+		}
+
+		else if (action == "run") {
+			await run(arg1, arg2);
+		}
+
+		else if (action == "kill") {
+			kill();
+		}
+
+		else if (action == "copy") {
+			await copy(arg1);
+		}
+
+		else if (action == "del") {
+			del();
+		}
 	}
 }
